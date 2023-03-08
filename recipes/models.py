47f72e980192315d1d13bf897225366a2391ff75
Wellisson Nunes <wellissonln@gmail.com>
2023-03-08 18:06:14 -0300

    feat: recipes model created

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField("Título", max_length=100)
+    ingredients = models.TextField("Ingredientes")
+    preparation_method = models.TextField("Modo de preparo")
+    chef = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
